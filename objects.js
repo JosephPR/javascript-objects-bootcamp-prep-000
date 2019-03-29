c0b13163377f04cf24fe5819f55cfdb3ca141a56
Joseph Reilly <reilly4181@gmail.com>
2019-03-29 19:16:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -10,4 +10,7 @@ function updatePlaylist(obj, key, value) {
  return obj
 }
 
-function removeFromPlaylist(playlist, artistName)
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.hendrix;   
+}
+  
